8b6c9fa62606a98218e6eee2eb6d9a024f46d42d
Harsimar <simarsingh24@gmail.com>
2017-04-03 16:47:00 +0530

    initial commit

---
+++ app/src/main/java/com/svnit/harsimar/imageprocessor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.svnit.harsimar.imageprocessor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
