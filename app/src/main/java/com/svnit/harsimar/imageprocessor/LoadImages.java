edd251cbfee4da6bbc4712d29d7e0e30ba834ac0
Harsimar <simarsingh24@gmail.com>
2017-04-08 04:44:51 +0530

    added listview and adapter

--- app/src/main/java/com/svnit/harsimar/imageprocessor/LoadImages.java
+++ app/src/main/java/com/svnit/harsimar/imageprocessor/LoadImages.java
@@ -3,11 +3,29 @@ package com.svnit.harsimar.imageprocessor;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.Firebase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoadImages extends AppCompatActivity {
-    private RecyclerView loadRecycler;
+    private ListView loadRecycler;
+    private DatabaseReference databaseReference;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +34,15 @@ public class LoadImages extends AppCompatActivity {
         ActionBar bar= getSupportActionBar();
         bar.setDisplayHomeAsUpEnabled(true);
 
-        loadRecycler=(RecyclerView)findViewById(R.id.load_recycler);
+        databaseReference= FirebaseDatabase.getInstance().getReference().child("ProcessedImages");
 
+        loadRecycler=(ListView) findViewById(R.id.load_recycler);
+
+        String[] fetchedItems={"harsimar","sinagh","is","here"};
+
+        ListAdapter folderAdapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,fetchedItems);
+
+        loadRecycler.setAdapter(folderAdapter);
 
 
 
