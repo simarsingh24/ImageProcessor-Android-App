72f695c87de438983bc88112569dd1290294895f
Harsimar <simarsingh24@gmail.com>
2017-04-07 11:07:36 +0530

    added location support

--- app/src/main/java/com/svnit/harsimar/imageprocessor/MainActivity.java
+++ app/src/main/java/com/svnit/harsimar/imageprocessor/MainActivity.java
@@ -1,24 +1,21 @@
 package com.svnit.harsimar.imageprocessor;
 
 import android.Manifest;
-import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.location.LocationManager;
-import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
-import android.provider.MediaStore;
-import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -33,41 +30,53 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.BufferUnderflowException;
-import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.List;
 
 import clarifai2.api.ClarifaiBuilder;
 import clarifai2.api.ClarifaiClient;
 import clarifai2.api.ClarifaiResponse;
-import clarifai2.api.ClarifaiUtil;
 import clarifai2.dto.input.ClarifaiInput;
 import clarifai2.dto.input.image.ClarifaiImage;
-import clarifai2.dto.model.ConceptModel;
 import clarifai2.dto.model.output.ClarifaiOutput;
 import clarifai2.dto.prediction.Concept;
 import okhttp3.OkHttpClient;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-import static android.view.View.GONE;
-import static android.view.View.TEXT_ALIGNMENT_VIEW_END;
-import static android.view.View.VISIBLE;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements
+        ConnectionCallbacks,
+        OnConnectionFailedListener,
+        LocationListener {
+
     private FloatingActionButton uploadFABBtn;
     final int PICK_IMAGE = 1;
-    final int REQUEST_IMAGE_CAPTURE=2;
-    final static String clientId ="TWLwx0Svio0V5WKuRZ1HejSVyYZtYTu8MydOT3yI";
-    final static String clientSecret ="1Lc3sttcMfHXzUcgZe3HVxkyWLHo77C3H6LmqxNs";
+    final int REQUEST_IMAGE_CAPTURE = 2;
+    final static String clientId = "TWLwx0Svio0V5WKuRZ1HejSVyYZtYTu8MydOT3yI";
+    final static String clientSecret = "1Lc3sttcMfHXzUcgZe3HVxkyWLHo77C3H6LmqxNs";
 
-    private final predictions_adapter adapter=new predictions_adapter();
+    private final predictions_adapter adapter = new predictions_adapter();
     private ImageView imageView;
     private RecyclerView recyclerView;
+
+
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private double currentLatitude;
+    private double currentLongitude;
+
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -77,6 +86,23 @@ public class MainActivity extends AppCompatActivity {
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             buildAlertMessageNoGps();
         }
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                // The next two lines tell the new client that “this” current class will handle connection stuff
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                //fourth line adds the LocationServices API endpoint from GooglePlayServices
+                .addApi(LocationServices.API)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+
     }
 
     private void buildAlertMessageNoGps() {
@@ -110,8 +136,8 @@ public class MainActivity extends AppCompatActivity {
                     1);
         }
 
-        TextView hintTv=(TextView)findViewById(R.id.hint_tv);
-        hintTv.setText(                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     "Please click on Action Button to continue!");
+        TextView hintTv = (TextView) findViewById(R.id.hint_tv);
+        hintTv.setText("Please click on Action Button to continue!");
 
 
         uploadFABBtn = (FloatingActionButton) findViewById(R.id.uploadFAB);
@@ -125,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
                 }
                 */
-                Log.d("harsimarSingh","outside");
+                Log.d("harsimarSingh", "outside");
             }
         });
     }
@@ -144,27 +170,27 @@ public class MainActivity extends AppCompatActivity {
         }
         switch (requestCode) {
             case PICK_IMAGE:
-                Log.d("hasimarSingh","picked");
+                Log.d("hasimarSingh", "picked");
 
                 //ProgressDialog pd=new ProgressDialog(MainActivity.this);
-                final ProgressDialog pd = ProgressDialog.show(this, "Working..", "Image Conversion", true,false);
+                final ProgressDialog pd = ProgressDialog.show(this, "Working..", "Image Conversion", true, false);
 
-                imageView=(ImageView)findViewById(R.id.main_image_view);
-                Uri uri=data.getData();
+                imageView = (ImageView) findViewById(R.id.main_image_view);
+                Uri uri = data.getData();
                 imageView.setImageURI(uri);
                 Handler handler = new Handler(Looper.getMainLooper());
 
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        final byte[] imageBytes = getImageByte(MainActivity.this,data);
+                        final byte[] imageBytes = getImageByte(MainActivity.this, data);
                         if (imageBytes != null) {
                             onImagePicked(imageBytes);
                         }
-                        Log.d("harsimarSingh","inside");
-                    pd.dismiss();
+                        Log.d("harsimarSingh", "inside");
+                        pd.dismiss();
                     }
-                },100 );
+                }, 100);
                 break;
         }
     }
@@ -196,23 +222,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     private void onImagePicked(final byte[] imageBytes) {
 
-        final ProgressDialog mProgress=new ProgressDialog(MainActivity.this);
+        final ProgressDialog mProgress = new ProgressDialog(MainActivity.this);
         mProgress.setTitle("Contacting API");
         mProgress.setMessage("Please Wait...");
         mProgress.show();
-        imageView=(ImageView)findViewById(R.id.main_image_view);
-        final TextView hint=(TextView)findViewById(R.id.hint_tv);
+        imageView = (ImageView) findViewById(R.id.main_image_view);
+        final TextView hint = (TextView) findViewById(R.id.hint_tv);
         uploadFABBtn.setEnabled(false);
         adapter.setData(Collections.<Concept>emptyList());
 
-        new AsyncTask<Void,Void,ClarifaiResponse<List<ClarifaiOutput<Concept>>>>(){
+        new AsyncTask<Void, Void, ClarifaiResponse<List<ClarifaiOutput<Concept>>>>() {
 
             @Override
             protected ClarifaiResponse<List<ClarifaiOutput<Concept>>> doInBackground(Void... params) {
-                Log.d("harsimarSingh","inside Async");
+                Log.d("harsimarSingh", "inside Async");
 
                 ClarifaiClient client = new ClarifaiBuilder(clientId, clientSecret)
                         .client(new OkHttpClient()).buildSync();
@@ -231,7 +256,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if (!response.isSuccessful()) {
 
-                    Toast.makeText(MainActivity.this,"Problem Contacting API",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Problem Contacting API", Toast.LENGTH_SHORT).show();
                     adapter.setData(Collections.<Concept>emptyList());
                     uploadFABBtn.setEnabled(true);
                     return;
@@ -239,7 +264,7 @@ public class MainActivity extends AppCompatActivity {
                 hint.setText("Predicted Results");
                 final List<ClarifaiOutput<Concept>> clarifaiOutputs = response.get();
                 if (clarifaiOutputs.isEmpty()) {
-                    Toast.makeText(MainActivity.this,"Empty Response From API",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Empty Response From API", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
@@ -252,4 +277,82 @@ public class MainActivity extends AppCompatActivity {
         }.execute();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        super.onPause();
+        Log.v(this.getClass().getSimpleName(), "onPause()");
+
+        //Disconnect from API onPause()
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        } else {
+            //If everything went fine lets get latitude and longitude
+            currentLatitude = location.getLatitude();
+            currentLongitude = location.getLongitude();
+
+            Toast.makeText(this, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+         /*
+             * Google Play services can resolve some errors it detects.
+             * If the error has a resolution, try sending an Intent to
+             * start a Google Play services activity that can resolve
+             * error.
+             */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                    /*
+                     * Thrown if Google Play services canceled the original
+                     * PendingIntent
+                     */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+                /*
+                 * If no resolution is available, display a dialog to the
+                 * user with the error.
+                 */
+            Log.e("Error", "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+
+        Toast.makeText(this, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+    }
 }
