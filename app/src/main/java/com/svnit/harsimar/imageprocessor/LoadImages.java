6ce4c13d0616f3faa160a3bcebf7dee9e43eb9ef
Harsimar <simarsingh24@gmail.com>
2017-04-08 04:17:26 +0530

    added back button in LoadImages java file

--- app/src/main/java/com/svnit/harsimar/imageprocessor/LoadImages.java
+++ app/src/main/java/com/svnit/harsimar/imageprocessor/LoadImages.java
@@ -1,7 +1,9 @@
 package com.svnit.harsimar.imageprocessor;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class LoadImages extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class LoadImages extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_images);
+
+        ActionBar bar= getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
