51456f8efe74369431c2c2c9490397327801d8ff
Harsimar <simarsingh24@gmail.com>
2017-04-08 06:44:53 +0530

    added back button

--- app/src/main/java/com/svnit/harsimar/imageprocessor/FetchImages.java
+++ app/src/main/java/com/svnit/harsimar/imageprocessor/FetchImages.java
@@ -1,6 +1,7 @@
 package com.svnit.harsimar.imageprocessor;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -16,11 +17,14 @@ public class FetchImages extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fetch_images);
         Bundle sentData= getIntent().getExtras();
+        ActionBar bar= getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
 
         folderNameString=sentData.getString(predictions_adapter.LABEL_KEY);
         imageRecycler=(RecyclerView)findViewById(R.id.images_recycler);
         folderName=(TextView)findViewById(R.id.folder_name_tv);
 
         folderName.setText(folderNameString);
+
     }
 }
